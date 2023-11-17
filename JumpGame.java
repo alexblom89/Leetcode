d36da1d3c496a6466c41b2985ab6f01680b6bb6a
Alex Blom <alexblom1989@gmail.com>
2023-11-16 22:17:53 -0500

    BuyAndSellStockII sln, JumpGame wip

---
+++ JumpGame.java
@@ -0,0 +1,11 @@
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int distance = nums[0];
+        int elements = nums.length;
+        for (int i = 1; i < distance; i++)
+        {
+
+        }
+    }
+
+}
