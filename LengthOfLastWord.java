61077d426809ecbc24bda23711d14751450b96d5
Alex Blom <alexblom1989@gmail.com>
2023-11-21 18:46:25 -0500

    LongestCommonPrefix

---
+++ LengthOfLastWord.java
@@ -0,0 +1,6 @@
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        String[] strings = s.trim().split(" ");
+        return strings[strings.length - 1].length();
+    }
+}
