6592c15843cd1add0d7f112039c7b3ed98bc94c9
Alex Blom <alexblom1989@gmail.com>
2023-12-07 20:11:38 -0500

    RemoveDupFromSortedArray2

--- RemoveDupFromSortedArray.java
+++ RemoveDupFromSortedArray.java
@@ -10,16 +10,36 @@ public class RemoveDupFromSortedArray {
         return i;
     }
 
+    /**
+     * https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/?envType=study-plan-v2&envId=top-interview-150
+     * Same as above but leave 2 of each char if more than 2 exist.
+     * @param nums
+     * @return
+     */
     public int removeDuplicates2(int[] nums) {
-        int i = 0;
-        for (int j = 1; j < nums.length; j++)
-        {
-            if (nums[i] != nums[j])
-            {
-                i++;
-                nums[i] = nums[j];
+        //define at most k times of duplicate numbers
+        final int k = 2;
+
+        //check if it is an empty array
+        if(nums.length == 0) return 0;
+
+        //start pointer of new array
+        int m = 1;
+
+        // count the time of duplicate numbers occurence
+        int count = 1;
+
+        for(int i = 1; i < nums.length; ++i) {
+            if(nums[i] == nums[i - 1]) {
+                if(count < k) {
+                    nums[m++] = nums[i];
+                }
+                count++;
+            } else {
+                count = 1;
+                nums[m++] = nums[i];
             }
         }
-        return i + 1;
+        return m;
     }
 }
