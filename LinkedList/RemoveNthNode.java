4a1098288af054514ceba5024debfe3fb0686232
Alex Blom <alexblom1989@gmail.com>
2024-01-22 15:30:17 -0500

    RemoveNthNode

---
+++ LinkedList/RemoveNthNode.java
@@ -0,0 +1,83 @@
+package LinkedList;
+
+/**
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+ */
+
+ //Definition for singly-linked list.
+ class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class RemoveNthNode {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = new ListNode();
+        start.next = head;
+
+        //Two pointers.
+        ListNode current = head;
+        ListNode previous = start;
+        int size = 0;
+
+        //Get list size.
+        while (current != null)
+        {
+            current = current.next;
+            size++;
+        }
+
+        //Deal with edgecase, if size and n are the same, the answer is always position 1. 
+        if (size == n)
+        {
+            return start.next.next;
+        }
+
+        int removeIndex = size - n;
+        current = head;
+
+        for (int i = 0; i < removeIndex; i++)
+        {
+            previous = current;
+            current = current.next;
+            if (i == removeIndex - 1)
+            {
+                previous.next = current.next;
+            }
+        }
+        
+        return start.next;
+    }
+
+    /**
+     * More concise solution with fast and slow pointers. 
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode fast = head, slow = head;
+
+        for (int i = 0; i < n; i++)
+        {
+            fast = fast.next;
+        } 
+
+        if (fast == null)
+        {
+            return head.next;
+        } 
+
+        while (fast.next != null) 
+        {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
+
